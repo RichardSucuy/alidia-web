636075e28184de5143cba346567c805595470c8d
RichardSucuy <richarsucuy@hotmail.com>
2026-02-07 20:32:39 -0500

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ChatWidget } from "@/components/chat/ChatWidget";
+import { Navbar } from "@/components/layout/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <header>
+          <Navbar />
+        </header>
+
         {children}
         {/* Chatbot global */}
         <ChatWidget />
